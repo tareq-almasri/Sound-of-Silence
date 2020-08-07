f31c8d27697824563667c268bd7f6e7e0a10e82f
tareq-almasri <talmasreq@gmail.com>
2020-08-07 23:15:35 +0200

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,51 @@
+class Music {
+  constructor() {
+    this.apiURL = `https://search-itunes.now.sh/?term=${this.searchText}&media=music`;
+    this.musicArr;
+    this.init();
+  }
 
-const list = document.querySelector(".box");
+  init() {
+    this.getData();
+  }
 
+  async getData() {
+    const data = fetch(this.apiURL);
+    const res = await data;
+    const resJSON = await res.json();
+    this.musicArr = resJSON.results;
+    this.renderTracks(this.musicArr);
+    this.sorting();
+  }
 
-const renderTracks=(tracks)=>{
-    list.innerHTML = '';
-tracks.forEach(x => {
-    const minutes = Math.floor(x.trackTimeMillis / 60000);
-    let seconds = Math.ceil(x.trackTimeMillis / 1000 % 60);
-    seconds<10?seconds='0'+seconds:seconds;
-    let time=minutes+':'+seconds;
-   
-    
-  list.innerHTML += 
-  `<div class='box2'>
+  renderTracks(tracks = []) {
+    const list = document.querySelector(".box");
+    list.innerHTML = "";
+    tracks.forEach(x => {
+      const minutes = Math.floor(x.trackTimeMillis / 60000);
+      let seconds = Math.ceil((x.trackTimeMillis / 1000) % 60);
+      seconds < 10 ? (seconds = "0" + seconds) : seconds;
+      let time = minutes + ":" + seconds;
+
+      list.innerHTML += `<div class='box2'>
    <div class='card text-white bg-dark'>
    <div class='card-body'>
    <div class='row no-gutters'>
         <div class='col'><img src='${x.artworkUrl100}'></div>
      
             <div class='col'>
-               <small class='text-muted'>ARTIST</small> <a href=${x.trackViewUrl} target='_blank'><i class="fab fa-apple"></i></a><br>
+               <small class='text-muted'>ARTIST</small> <a href=${
+                 x.trackViewUrl
+               } target='_blank'><i class="fab fa-apple"></i></a><br>
                ${x.artistName} 
             </div>
      
     </div>
     <small class='text-muted'>ALBUM</small><br>
      <p class='card-text'> ${x.collectionName}</p>
-    <div class='row no-gutters h4'><p class='card-text'>${x.trackName} </p></div>
+    <div class='row no-gutters h4'><p class='card-text'>${
+      x.trackName
+    } </p></div>
     </div>
    <div class='card-footer'><p class='show'>show more <i class="fas fa-caret-down"></i></p></div>
    </div>
@@ -49,67 +68,79 @@ tracks.forEach(x => {
    </div>
    </div>`;
 
-    let showMore = document.querySelectorAll('.show');
+      let showMore = document.querySelectorAll(".show");
 
-    showMore.forEach(x => x.addEventListener('click', () => {
-        x.parentElement.parentElement.nextElementSibling.classList.toggle('toggle')
-    }))
+      showMore.forEach(x =>
+        x.addEventListener("click", () => {
+          x.parentElement.parentElement.nextElementSibling.classList.toggle(
+            "toggle"
+          );
+        })
+      );
+    });
+  }
+  sorting() {
+    const sort = document.querySelector(".sort");
 
-   
-  
-});
-   
-}
+    sort.addEventListener("click", () => {
+      sort.nextElementSibling.classList.toggle("toggle");
+    });
 
-const sort = document.querySelector('.sort');
+    const price = document.querySelector(".price");
 
-sort.addEventListener('click', () => {
-    sort.nextElementSibling.classList.toggle('toggle');
-})
+    price.addEventListener("click", () => {
+      let sorted = this.musicArr.sort((a, b) => a.trackPrice - b.trackPrice);
 
-const price=document.querySelector('.price');
+      this.renderTracks(sorted);
+    });
+    const length = document.querySelector(".length");
 
-price.addEventListener('click',()=>{
-    let sorted=music.sort((a,b)=>a.trackPrice-b.trackPrice);
+    length.addEventListener("click", () => {
+      let sorted = this.musicArr.sort(
+        (a, b) => a.trackTimeMillis - b.trackTimeMillis
+      );
 
-    renderTracks(sorted);
-})
-const length = document.querySelector('.length');
+      this.renderTracks(sorted);
+    });
+    const release = document.querySelector(".release");
 
-length.addEventListener('click', () => {
-    let sorted = music.sort((a, b) => a.trackTimeMillis - b.trackTimeMillis);
+    release.addEventListener("click", () => {
+      let sorted = this.musicArr.sort(
+        (a, b) =>
+          new Date(a.releaseDate.substr(0, 10)) -
+          new Date(b.releaseDate.substr(0, 10))
+      );
 
-    renderTracks(sorted);
-})
-const release = document.querySelector('.release');
+      this.renderTracks(sorted);
+    });
+    const release2 = document.querySelector(".release2");
 
-release.addEventListener('click', () => {
-    let sorted = music.sort((a, b) => new Date(a.releaseDate.substr(0,10))-new Date(b.releaseDate.substr(0,10)));
+    release2.addEventListener("click", () => {
+      let sorted = this.musicArr.sort(
+        (a, b) =>
+          new Date(b.releaseDate.substr(0, 10)) -
+          new Date(a.releaseDate.substr(0, 10))
+      );
+      this.renderTracks(sorted);
+    });
+    searchText()
+    document.querySelector("input").addEventListener("keyup", e => {
+      const keyword = e.target.value.toLocaleLowerCase();
 
-    renderTracks(sorted);
-})
-const release2=document.querySelector('.release2');
+      let filteredMusic = this.musicArr.filter(
+        track =>
+          track.artistName.toLocaleLowerCase().includes(keyword) ||
+          track.trackName.toLocaleLowerCase().includes(keyword)
+      );
 
-release2.addEventListener('click',()=>{
-    let sorted = music.sort((a, b) => new Date(b.releaseDate.substr(0, 10)) - new Date(a.releaseDate.substr(0, 10)));
-    renderTracks(sorted);
-})
-
-document.querySelector("input").addEventListener("keyup", (e) => {
-    const keyword = e.target.value.toLocaleLowerCase();
-
-    let filteredMusic = music.filter(
-        track => track.artistName.toLocaleLowerCase().includes(keyword)
-            || track.trackName.toLocaleLowerCase().includes(keyword))
-
-    if (filteredMusic.length === 0) {
+      if (filteredMusic.length === 0) {
         list.innerHTML = "No tracks were found";
         return;
-    }
+      }
 
-    renderTracks(filteredMusic);
-})
+      this.renderTracks(filteredMusic);
+    });
+  }
+}
 
-renderTracks(music);
-
-
+const myMusic = new Music();
